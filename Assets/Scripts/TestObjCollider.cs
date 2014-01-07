803b754dc5e312ec07900f5b27b2f182a8d02e0c
zwang87 <zw401@nyu.edu>
2014-01-07 14:30:42 -0500

    Optitrack controlled Mixed Reality
    Early version of Mixed Reality, working with optitrack

---
+++ Assets/Scripts/TestObjCollider.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestObjCollider : MonoBehaviour
+{
+	public static Dictionary<string, string> TestObjShaders = new Dictionary<string, string> ();
+
+	public void OnTriggerEnter (Collider other)
+	{
+		switch (other.tag) {
+		case "TestObj":
+			if (other.transform.parent != null)
+				return;
+			other.transform.parent = this.transform.parent; 
+			if (other.rigidbody != null)
+				other.rigidbody.useGravity = false;
+			TestObjShaders [other.name] = other.renderer.material.shader.name;
+//			other.renderer.material.shader = Shader.Find ("Transparent/Diffuse");
+			
+			Color c = other.renderer.material.color;
+			c.a = 0.6f;
+			other.renderer.material.color = c;
+			break;
+		}
+	}
+}
